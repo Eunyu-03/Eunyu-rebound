6d493aefdebc89fbe55ced19ece410e032d04e50
Eunyu <wjddmsdb03@gmail.com>
2025-09-04 14:14:40 +0900

    re

--- src/test/java/com/example/rebound/ReboundApplicationTests.java
+++ src/test/java/com/example/rebound/ReboundApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ReboundApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
